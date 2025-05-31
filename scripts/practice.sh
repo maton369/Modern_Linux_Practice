9b5b097756cffaf53c32e734335b8cf71d6c4f5c
maton369 <ysk901085@gmail.com>
2025-05-31 10:25:50 +0900

    Add: 出力結果を残せるようにした

--- scripts/practice.sh
+++ scripts/practice.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-dmidecode
+echo "Collecting system info..." > /scripts/output.log
+date >> /scripts/output.log
+uname -a >> /scripts/output.log
+dmidecode >> /scripts/output.log 2>&1
+echo "Done." >> /scripts/output.log
